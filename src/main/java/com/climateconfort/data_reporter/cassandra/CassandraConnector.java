362c408ce970266a83dc005c5d174c43b12246e6
Diogo_S <diosgodsousa@gmail.com>
2024-05-23 08:16:12 +0200

    cassandra connector klasea eginda

--- src/main/java/com/climateconfort/data_reporter/cassandra/CassandraConnector.java
+++ src/main/java/com/climateconfort/data_reporter/cassandra/CassandraConnector.java
@@ -1,67 +1,83 @@
 package com.climateconfort.data_reporter.cassandra;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.api.core.cql.SimpleStatement;
 
-@PropertySource("application.properties")
-public class CassandraConnector {
-    Properties prop;
-    @Value("${spring.datasource.cassandra_ip}") private String CASSANDRA_HOST;
-    
-    private final int CASSANDRA_PORT = 0000;
-    private final String CASSANDRA_DATABASE = "database";
-    private final String CASSANDRA_USERNAME = "user";
-    private final String CASSANDRA_PASSWORD = "user";
-    private final String QUERY = "SELECT columna1, columna2 FROM tabla_de_ejemplo";
-    Map<String, Integer[]> parameters;
+public class CassandraConnector 
+{
+    private Properties prop;
+    private int enpresa_id;
+    private final String FICHEROPROPIEDADES = "src\\main\\java\\com\\climateconfort\\resources\\application.properties";
+    private final String ERAIKIN_QUERY = "SELECT id FROM climate_confort.Eraikina WHERE enpresa_id=";
+    private final String GELA_QUERY = "SELECT id, parametro_min, parametro_max, eraikina_id FROM climate_confort.Gela WHERE eraikina_id=";
+    private Map<Integer, Float[]> gelaParameters;
+    private Map<Integer, Map<Integer, Float[]>> eraikinak;
 
     /**
-     * @brief   CassandraConnector class constructor
+     * @throws  IOException 
+     * @throws  FileNotFoundException 
+     * 
+     * @params  enpresa_id
+     * 
+     * @brief   CassandraConnector konstruktorea
      */
-    public CassandraConnector() 
+    public CassandraConnector(int enpresa_id) throws FileNotFoundException, IOException 
     {
+        this.enpresa_id = enpresa_id;
         prop = new Properties();
-        parameters = new HashMap<>();
+        prop.load(new FileInputStream(FICHEROPROPIEDADES));
+        gelaParameters = new HashMap<>();
+        eraikinak = new HashMap<>();
     }
 
     /**
-     * @brief   
+     * @brief   Parametroak lortu Cassandra datu basetik eta Map bat sortu eraikin eta gela guztiekin.
      */
-    public void getParameters() 
+    public Map<Integer, Map<Integer, Float[]>> getParameters() 
     {
-        /* Usar uno de los metodos para obtener la IP */
-        System.out.println(prop.getProperty("spring.datasource.cassandra_ip"));
-        System.out.println(CASSANDRA_HOST);
-
+        /* Obtener propiedades */
+        int cassandra_port = Integer.parseInt(prop.getProperty("spring.datasource.cassandra_port"));
+        String cassandra_host = prop.getProperty("spring.datasource.cassandra_ip");
+        String cassandra_user = prop.getProperty("spring.datasource.cassandra_user");
+        String cassandra_password = prop.getProperty("spring.datasource.cassandra_password");
+        String cassandra_database = prop.getProperty("spring.datasource.cassandra_database");
 
         try (CqlSession session = CqlSession.builder()
-            .addContactPoint(new InetSocketAddress(CASSANDRA_HOST, CASSANDRA_PORT))
-            .withLocalDatacenter(CASSANDRA_DATABASE)  // Reemplaza con el nombre de tu datacenter
-            .withAuthCredentials(CASSANDRA_USERNAME, CASSANDRA_PASSWORD)
-            .withKeyspace("nombre_del_keyspace")  // Reemplaza con el nombre de tu keyspace si deseas conectarte a uno específico
+            .addContactPoint(new InetSocketAddress(cassandra_host, cassandra_port))
+            .withLocalDatacenter(cassandra_database)
+            .withAuthCredentials(cassandra_user, cassandra_password)
             .build())
         {
-            ResultSet resultSet = session.execute(SimpleStatement.newInstance(QUERY));
 
-            for (Row row : resultSet) 
-            {    
-                /* Definir tablas de contenido */
-                int paramtero = row.getInt("parametro1");
-                String paramtero2 = row.getString("parametro1");
+            ResultSet raiknResultSet = session.execute(SimpleStatement.newInstance(ERAIKIN_QUERY + enpresa_id));
+            Float[] temporal_parameters = new Float[2];
 
-                /* Primero printear valores, para ver si funciona */
+            for (Row eraikina_row : raiknResultSet) 
+            {
+                int eraikina_id = eraikina_row.getInt("id");   
+                ResultSet gelaResultSet = session.execute(SimpleStatement.newInstance(GELA_QUERY + eraikina_id));
+                
+                for (Row gela_row : gelaResultSet) 
+                {
+                    int gela_id = gela_row.getInt("id");
+                    temporal_parameters[0] = gela_row.getFloat("parametro_min");
+                    temporal_parameters[1] = gela_row.getFloat("parametro_max");
+            
+                    gelaParameters.put(gela_id, temporal_parameters);
+                }
+                eraikinak.put(eraikina_id, gelaParameters);
             }
+            return eraikinak;
         }
     }
-
 }
