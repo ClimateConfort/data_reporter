95ff36b5191f921f445377b12fc68698231b6ac9
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-05-25 18:17:34 +0200

    Add a working example

--- src/main/java/com/climateconfort/data_reporter/Main.java
+++ src/main/java/com/climateconfort/data_reporter/Main.java
@@ -1,7 +1,26 @@
 package com.climateconfort.data_reporter;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.climateconfort.data_reporter.data_collection.DataReceiver;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/resources/application.properties"));
+        List<String> publisherIdList = new ArrayList<>();
+        publisherIdList.add("1-1");
+        DataReceiver dataReceiver = new DataReceiver(properties, publisherIdList);
+
+        (new Thread(() -> dataReceiver.subscribe())).start();
+
+        while (true) {
+            dataReceiver.getSensorData().ifPresent(System.out::println);
+        }
     }
 }
