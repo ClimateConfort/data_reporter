6f60ad210c2924d38352224976320b1658bd760d
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-05-29 15:57:40 +0200

    Add avro serialization

---
+++ src/main/java/com/climateconfort/data_reporter/avro/AvroSerializer.java
@@ -0,0 +1,47 @@
+package com.climateconfort.data_reporter.avro;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.avro.file.CodecFactory;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import com.climateconfort.common.SensorData;
+
+public class AvroSerializer {
+
+    public AvroSerializer() {}
+
+    private static final DatumWriter<SensorDataAvro> sensorDatumWriter = new SpecificDatumWriter<>(
+            SensorDataAvro.class);
+
+    public static ByteArrayOutputStream packToAvroFile(List<SensorData> sensorDataList) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (DataFileWriter<SensorDataAvro> dataFileWriter = new DataFileWriter<>(sensorDatumWriter)) {
+            dataFileWriter.setCodec(CodecFactory.zstandardCodec(22, true, true));
+            dataFileWriter.create(SensorDataAvro.SCHEMA$, outputStream);
+            for (SensorData sensorData : sensorDataList) {
+                SensorDataAvro sensorDataAvro = createSensorDataAvro(sensorData);
+                dataFileWriter.append(sensorDataAvro);
+            }
+        }
+
+        return outputStream;
+    }
+
+    private static SensorDataAvro createSensorDataAvro(SensorData sensorData) {
+        return SensorDataAvro.newBuilder()
+                .setUnixTime(sensorData.getUnixTime())
+                .setRoomId(sensorData.getRoomId())
+                .setBuildingId(sensorData.getBuildingId())
+                .setClientId(sensorData.getClientId())
+                .setTemperature(sensorData.getTemperature())
+                .setSoundLevel(sensorData.getSoundLevel())
+                .setHumidity(sensorData.getHumidity())
+                .setPressure(sensorData.getPressure())
+                .build();
+    }
+}
