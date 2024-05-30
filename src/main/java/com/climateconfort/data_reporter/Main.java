d2895bb476d04f2b5fc84c51278bfc9cbf40099d
Diogo_S <diosgodsousa@gmail.com>
2024-05-30 22:06:02 +0200

    Guztia funtzionala, cassandra testa amaitu gabe

--- src/main/java/com/climateconfort/data_reporter/Main.java
+++ src/main/java/com/climateconfort/data_reporter/Main.java
@@ -8,16 +8,14 @@ import com.climateconfort.data_reporter.cassandra.CassandraConnector;
 import com.climateconfort.data_reporter.cassandra.GCP;
 
 public class Main {
-    static CassandraConnector cs;
+    
     public static void main(String[] args) throws FileNotFoundException, IOException 
     {
         GCP gcp = new GCP();
         Map<String, String[]> map = gcp.listInstances();
 
         CassandraConnector cassandraConnector = new CassandraConnector(1, map);
-        //cassandraConnector.connect("35.238.37.29", 9042);
-        // cassandraConnector.connectKeyspace();
-        System.out.println(cassandraConnector.getSession().toString());
+        Map<Integer, Map<Integer, Map<String, Float[]>>> map2 = cassandraConnector.getParameters();
         cassandraConnector.close();
     }
 }
