2af8ef032e4957375669dd42bf22c4257c6e4bfc
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-05-30 12:25:29 +0200

    Add action sender

---
+++ src/main/java/com/climateconfort/data_reporter/actions/ActionSender.java
@@ -0,0 +1,32 @@
+package com.climateconfort.data_reporter.actions;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeoutException;
+
+import com.climateconfort.common.Constants;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class ActionSender {
+
+    private final ConnectionFactory connectionFactory;
+
+    public ActionSender(Properties properties) {
+        this.connectionFactory = new ConnectionFactory();
+        this.connectionFactory.setHost(properties.getProperty("rabbitmq_server_ip", "localhost"));
+        this.connectionFactory.setPort(Integer.parseInt(properties.getProperty("rabbitmq_server_port", "5672")));
+        this.connectionFactory.setUsername(properties.getProperty("rabbitmq_server_user", "guest"));
+        this.connectionFactory.setPassword(properties.getProperty("rabbitmq_server_password", "guest"));
+    }
+
+    public void publish(long roomId, long buildingId, String action) throws IOException, TimeoutException {
+        try (Connection connection = connectionFactory.newConnection();
+                Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(Constants.SENSOR_ACTION_EXCHANGE, "topic");
+            channel.basicPublish(Constants.SENSOR_ACTION_EXCHANGE, String.format(buildingId + "." + roomId), null,
+                    action.getBytes());
+        }
+    }
+}
