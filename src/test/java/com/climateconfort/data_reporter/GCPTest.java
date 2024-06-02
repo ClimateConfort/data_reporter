42207e3c1552a74ef17270cfef9d0514292fc7ed
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-06-03 00:25:32 +0200

    Add tests

--- src/test/java/com/climateconfort/data_reporter/GCPTest.java
+++ src/test/java/com/climateconfort/data_reporter/GCPTest.java
@@ -1,4 +1,4 @@
-package com.climateconfort;
+package com.climateconfort.data_reporter;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
