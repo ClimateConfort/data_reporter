028286fa5a672f663526c5b15413a5ee08e9970f
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-06-02 17:42:41 +0200

    Convert to object mapping

--- src/main/java/com/climateconfort/data_reporter/cassandra/domain/parametroa/dao/ParametroaDao.java
+++
@@ -1,9 +0,0 @@
-package com.climateconfort.data_reporter.cassandra.domain.parametroa.dao;
-
-import java.util.List;
-
-import com.climateconfort.data_reporter.cassandra.domain.parametroa.model.Parametroa;
-
-public interface ParametroaDao {
-    List<Parametroa> findByGelaId(int gelaId);
-}
