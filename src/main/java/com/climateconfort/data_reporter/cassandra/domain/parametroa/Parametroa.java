ab9277ae4e409562859bdcbe65fd13dde032ea52
enaut-genua <enaut.genua@alumni.mondragon.edu>
2024-06-06 14:59:01 +0200

    Clean

--- src/main/java/com/climateconfort/data_reporter/cassandra/domain/parametroa/Parametroa.java
+++ src/main/java/com/climateconfort/data_reporter/cassandra/domain/parametroa/Parametroa.java
@@ -1,6 +1,5 @@
 package com.climateconfort.data_reporter.cassandra.domain.parametroa;
 
-import com.datastax.oss.driver.api.mapper.annotations.CqlName;
 import com.datastax.oss.driver.api.mapper.annotations.Entity;
 import com.datastax.oss.driver.api.mapper.annotations.PartitionKey;
 
